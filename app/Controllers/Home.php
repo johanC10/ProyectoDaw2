34da1f5cfbce00b6978bc1eb11b98388fea15798
johanC10 <jestupina@inscaparrella.cat>
2026-02-10 18:56:16 +0100

    proves

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -11,6 +11,6 @@ class Home extends BaseController
 
     /*public function index(): string
     {
-        return view('welcome_message');
+        return view('formulario/inicio_proceso');
     }*/
 }
