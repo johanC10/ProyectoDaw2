5f0b7867e37764fe71f092c4ef40e3577cf5f9f9
johanC10 <jestupina@inscaparrella.cat>
2026-02-20 15:19:06 +0100

    .

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -4,13 +4,13 @@ namespace App\Controllers;
 
 class Home extends BaseController
 {
-    public function index(): string
-    {
-        return view('public/inicio_proceso');
-    }
-
     /*public function index(): string
     {
         return view('public/inicio_proceso');
     }*/
+
+    public function index(): string
+    {
+        return view('private/auth/login_gestion');
+    }
 }
